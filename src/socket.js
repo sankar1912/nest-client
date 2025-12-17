b519392d1057a0b4e94f4564f9ca9f24fc6f1c91
sankarsnk <ksankar1912@gmail.com>
2025-12-17 22:55:19 +0530

    URL update

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:8000", {
+export const socket = io("https://nest-chat-server-d01j.onrender.com", {
   transports: ["websocket"],
 });
